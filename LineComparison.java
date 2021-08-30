00e91828d8a23aa94abdf01e199dd7732c20d357
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-08-30 16:30:22 +0530

    Wellcome To Line Comparison

---
+++ LineComparison.java
@@ -0,0 +1,11 @@
+package com.bl.linecomparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		//Print statement 
+		System.out.println("Wellcom to Line Comparison");
+
+	}
+
+}
